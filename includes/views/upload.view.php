162b49efde28a86cbab2f756557eb095db9f057b
Magnus Westhofen <magnuswesthofen@icloud.com>
2020-08-26 14:29:46 +0200

    test

--- includes/views/upload.view.php
+++ includes/views/upload.view.php
@@ -4,13 +4,13 @@
     $url = content_url().'/reactapps/'.$name.'/'; // getting the previous urls/paths;
 
     // check if url and path exists, then display information on the current build.
-    if ($name) { 
+    if ($name) {
         echo '<div style="margin:25px 0">';
-        echo '<strong>'.__('Current build files', 'react-app-shortcodes').':</strong><br /> '.$url;
+        echo '<strong>'.__('Current build files', 'wp-react-app-importer').':</strong><br /> '.$url;
         echo '</div>';
         echo '<hr />';
         echo '<div style="margin:25px 0">';
-        echo '<strong>'.__('Your shortcode', 'react-app-shortcodes').'</strong>:<br />';
+        echo '<strong>'.__('Your shortcode', 'wp-react-app-importer').'</strong>:<br />';
         echo '<input style="color: #666;width:400px;margin-top:5px;" type="text" disabled name="shortcode" value="[React-App id=&quot;'.$_GET['post'].'&quot;]" />';
         echo '</div>';
         echo '<hr />';
@@ -19,6 +19,6 @@
     // Start rendering the input field for uploading.
     ?>
     <div style="margin-bottom:25px">
-        <p><?php echo __('Upload a ZIP of your Creat-Reat App build here. Your previous build will be deleted.', 'react-app-shortcodes');?></p>
+        <p><?php echo __('Upload a ZIP of your Create-React App build here. Your previous build will be deleted.', 'wp-react-app-importer');?></p>
         <input type="file" id="react_app_uploader" name="react_app_uploader" value="" size="25" />
     </div>
